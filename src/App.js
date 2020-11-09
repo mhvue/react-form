5950e1be1794c759258cdc852df9483173856b8d
Mai Houa Vue <mhvue4@gmail.com>
2020-11-08 18:02:42 -0600

    deleted files from boiler plate not needed. add js files i will need to create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React form will go here</h1>
+        <h1>yooo</h1>
       </header>
     </div>
   );
