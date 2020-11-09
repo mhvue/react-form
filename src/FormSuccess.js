241271ad2062883577177f7e9f481e7c230adfc0
Mai Houa Vue <mhvue4@gmail.com>
2020-11-08 21:28:37 -0600

    add success msg once information have successfully gone through

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -1,1 +1,13 @@
 //this is for when form is sucessfully submited
+
+import React from "react";
+
+const FormSuccess =() => {
+    return(
+        <div className ="form-content-right">
+            <div className = "form-success">Success! We have received your request.</div>
+        </div>
+    )
+}
+
+export default FormSuccess;
